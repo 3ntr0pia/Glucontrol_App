061ff31a7b25091b1fef4fa9c1a9a3ac29071d4a
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2023-12-22 16:50:41 +0100

    Agregado 2 controllers
    Agregado  controller de personas y mediciones

---
+++ WebApiCadaGotaCuenta/WebApiCadaGotaCuenta/Services/GestorArchivosLocal.cs
@@ -0,0 +1,56 @@
+﻿namespace WebApiCadaGotaCuenta.Services
+{
+    public class GestorArchivosLocal : IGestorArchivos
+    {
+        private readonly IWebHostEnvironment env;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public GestorArchivosLocal(IWebHostEnvironment env,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            this.env = env;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task BorrarArchivo(string ruta, string carpeta)
+        {
+            if (ruta != null)
+            {
+                var nombreArchivo = Path.GetFileName(ruta);
+                string directorioArchivo = Path.Combine(env.WebRootPath, carpeta, nombreArchivo);
+
+                if (File.Exists(directorioArchivo))
+                {
+                    File.Delete(directorioArchivo);
+                }
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public async Task<string> EditarArchivo(byte[] contenido, string extension, string carpeta, string ruta,
+            string contentType)
+        {
+            await BorrarArchivo(ruta, carpeta);
+            return await GuardarArchivo(contenido, extension, carpeta, contentType);
+        }
+
+        public async Task<string> GuardarArchivo(byte[] contenido, string extension, string carpeta,
+            string contentType)
+        {
+            var nombreArchivo = $"{Guid.NewGuid()}{extension}";
+            string folder = Path.Combine(env.WebRootPath, carpeta);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string ruta = Path.Combine(folder, nombreArchivo);
+            await File.WriteAllBytesAsync(ruta, contenido);
+            var urlActual = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}";
+            var urlParaBD = Path.Combine(urlActual, carpeta, nombreArchivo).Replace("\\", "/");
+            return urlParaBD;
+        }
+    }
+}
