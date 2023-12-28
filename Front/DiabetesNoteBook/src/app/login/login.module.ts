fab61804dfebf7a9f79ace59a1a1f8219ae59a23
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2023-12-28 10:56:47 +0100

    .

--- Front/DiabetesNoteBook/src/app/login/login.module.ts
+++ Front/DiabetesNoteBook/src/app/login/login.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './components/form/form.component';
 import { FormsModule } from '@angular/forms';
-import { MainComponent } from './pages/main/main.component';
 import { InfoComponent } from './components/info/info.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
-  declarations: [FormComponent, MainComponent, InfoComponent],
+  declarations: [FormComponent, InfoComponent, MainComponent],
   imports: [CommonModule, FormsModule],
   exports: [MainComponent],
 })
