64139e1c24326d98bfab410746e77257784a971a
MocaiMocai <148332922+MocaiMocai@users.noreply.github.com>
2024-01-16 23:34:13 +0100

    error page

---
+++ Front/DiabetesNoteBook/src/app/shared/notfound/notfound.component.ts
@@ -0,0 +1,16 @@
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as THREE from 'three';
+import { gsap, Power4 } from 'gsap';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css'],
+})
+export class NotfoundComponent {}
--- Front/DiabetesNoteBook/src/app/shared/shared.module.ts
+++ Front/DiabetesNoteBook/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { SharedRoutingModule } from './shared-routing.module';
 
 @NgModule({
-  declarations: [ModalComponent],
-  imports: [CommonModule],
+  declarations: [ModalComponent, NotfoundComponent],
+  imports: [CommonModule,SharedRoutingModule],
   exports: [ModalComponent],
 })
 export class SharedModule {}
