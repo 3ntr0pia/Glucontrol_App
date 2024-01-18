532f589515d8bf1e722919036849972c4a1dbec3
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2024-01-18 20:36:24 +0100

    ajustes

--- Front/DiabetesNoteBook/src/app/shared/shared.module.ts
+++ Front/DiabetesNoteBook/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { CalculadoraImcComponent } from './calculadora-imc/calculadora-imc.component';
 import { FormsModule } from '@angular/forms';
+import { GeneradorAvatarComponent } from './generador-avatar/generador-avatar.component';
 
 @NgModule({
-  declarations: [ModalComponent, CalculadoraImcComponent],
+  declarations: [ModalComponent, CalculadoraImcComponent,GeneradorAvatarComponent],
   imports: [CommonModule , FormsModule],
-  exports: [ModalComponent,CalculadoraImcComponent],
+  exports: [ModalComponent,CalculadoraImcComponent,GeneradorAvatarComponent],
 })
 export class SharedModule {}
--- Front/DiabetesNoteBook/src/app/user/components/vademecum/vademecum.component.ts
+++ Front/DiabetesNoteBook/src/app/user/components/vademecum/vademecum.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IMedicamento } from '../../interfaces/medicamento.interface';
+import { VademecumService } from 'src/app/services/vademecum.service';
 
 @Component({
   selector: 'app-vademecum',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./vademecum.component.css']
 })
 export class VademecumComponent {
+  med : string = "lorazepam, Ibuprofeno, Paracetamol, Omeprazol, Amoxicilina, Diazepam, Trankimazin, Alprazolam, Dalsy, Nolotil,"
+
+  arrayMed : IMedicamento[] = []
+  constructor( private vademecum : VademecumService) { }
+  
+  ngOnInit(): void {
+    this.crearArrayMedicamentos();
+  }
+  
+  
+  crearArrayMedicamentos() : void {
+    let nombresMedicamentos = this.med.split(',').map(m => m.trim()).filter(m => m);
+    nombresMedicamentos.forEach(nombre => {
+      this.vademecum.getMedicamentoInfo(nombre).subscribe(
+        (res ) => {
+          
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    });
+  }
 
 }
