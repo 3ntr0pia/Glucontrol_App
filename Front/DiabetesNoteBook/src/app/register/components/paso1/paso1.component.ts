e7a7bbc0dab92886cd888b772867893caf0e6a4d
JavierOrtiz <112349811+xaby19@users.noreply.github.com>
2024-01-18 11:27:16 +0100

    .

--- Front/DiabetesNoteBook/src/app/register/components/paso1/paso1.component.ts
+++ Front/DiabetesNoteBook/src/app/register/components/paso1/paso1.component.ts
@@ -2,63 +2,60 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { Sexo, Actividad, TipoDiabetes } from '../../interfaces/register.enum';
 import { IRegister } from '../../interfaces/register.interface';
 import { AvatarService } from 'src/app/services/avatar.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'register-paso1',
   templateUrl: './paso1.component.html',
-  styleUrls: ['./paso1.component.css']
+  styleUrls: ['./paso1.component.css'],
 })
 export class Paso1Component {
-
   @Output() siguientePaso = new EventEmitter<IRegister>();
 
-  @Input() datosRegistro : IRegister = {
+  @Input() datosRegistro: IRegister = {
     username: '',
     avatar: '',
-    nombre: "",
-    apellido: "",
-    apellido2: "",
-    email: "",
-    password: "",
-    password2: "",
-    mediciones : {
-      edad : 0,
-      peso : 0,
-      altura : 0,
-      sexo : Sexo.hombre,
-      actividad : Actividad.sedentario,
-      tipoDiabetes : {
-        tipo : TipoDiabetes.tipo1,
-        medicacion: "",
-        insulina: false
-      }
-    }
+    nombre: '',
+    apellido: '',
+    apellido2: '',
+    email: '',
+    password: '',
+    password2: '',
+    mediciones: {
+      edad: 0,
+      peso: 0,
+      altura: 0,
+      sexo: Sexo.hombre,
+      actividad: Actividad.sedentario,
+      tipoDiabetes: {
+        tipo: TipoDiabetes.tipo1,
+        medicacion: '',
+        insulina: false,
+      },
+    },
   };
 
-  password2 : string = "";
-  avatar : string = '';
-  hasError:boolean = false;
-  defaultAvatar : string ="assets/avatar.png";
-
+  password2: string = '';
+  avatar: string = '';
+  hasError: boolean = false;
+  defaultAvatar: string = 'assets/avatar.png';
 
   constructor(private avatarService: AvatarService) {}
 
   generarAvatar() {
-    
     this.datosRegistro.avatar = this.avatarService.getRandomAvatar();
-    
   }
-  
 
   formularioInvalido(): boolean {
-    return !this.datosRegistro.nombre ||
-           !this.datosRegistro.username ||
-           !this.datosRegistro.apellido ||
-           !this.datosRegistro.apellido2 ||
-           !this.datosRegistro.email ||
-           !this.datosRegistro.password ||
-           !this.datosRegistro.avatar ;
-           
+    return (
+      !this.datosRegistro.nombre ||
+      !this.datosRegistro.username ||
+      !this.datosRegistro.apellido ||
+      !this.datosRegistro.apellido2 ||
+      !this.datosRegistro.email ||
+      !this.datosRegistro.password ||
+      !this.datosRegistro.avatar
+    );
   }
   validarPassword(password: string): boolean {
     const minLength = 8;
@@ -66,8 +63,13 @@ export class Paso1Component {
     const hasLowerCase = /[a-z]/.test(password);
     const hasNumbers = /\d/.test(password);
     const hasSpecialChar = /[\W_]/.test(password);
-  
-    return password.length >= minLength && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+
+    return (
+      password.length >= minLength &&
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumbers &&
+      hasSpecialChar
+    );
   }
-  
 }
