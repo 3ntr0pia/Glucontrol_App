c119d6cbedf9f72f76be892e5c9ba8a36e64fce1
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2024-01-24 15:07:49 +0100

    correcciones

--- Front/DiabetesNoteBook/src/app/user/user.module.ts
+++ Front/DiabetesNoteBook/src/app/user/user.module.ts
@@ -1,17 +1,22 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+//Components
+import { VademecumComponent } from './components/vademecum/vademecum.component';
+import { MedicionesComponent } from './components/mediciones/mediciones.component';
+import { MisDatosComponent } from './components/mis-datos/mis-datos.component';
+import { AyudaComponent } from './components/ayuda/ayuda.component';
 import { MainComponent } from './pages/main/main.component';
 import { NavbarComponent } from './global/navbar/navbar.component';
 
-import { FormsModule } from '@angular/forms';
-import { UserRoutingModule } from './user-routing.module';
-import { MisDatosComponent } from './components/mis-datos/mis-datos.component';
-
-import { AyudaComponent } from './components/ayuda/ayuda.component';
-import { SharedModule } from '../shared/shared.module';
-import { VademecumComponent } from './components/vademecum/vademecum.component';
+// Pipes
 import { FiltroGenericosPipe } from '../pipes/filtro-genericos.pipe';
 import { FiltroSinRecetaPipe } from '../pipes/filtro-sin-receta.pipe';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -23,8 +28,18 @@ import { FiltroSinRecetaPipe } from '../pipes/filtro-sin-receta.pipe';
     VademecumComponent,
     FiltroGenericosPipe,
     FiltroSinRecetaPipe,
+
+    MedicionesComponent,
   ],
-  imports: [CommonModule, UserRoutingModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    FormsModule,
+    SharedModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
   exports: [MainComponent],
 })
 export class UserModule {}
