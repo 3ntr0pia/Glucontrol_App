7ff607bf63e55a2d818b0889b76866a9fda2bd55
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2024-01-30 10:52:27 +0100

    fixeado recuperar pass

--- Front/DiabetesNoteBook/src/app/services/recordar-pass.service.ts
+++ Front/DiabetesNoteBook/src/app/services/recordar-pass.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IRecover } from '../interfaces/loginResponse.interface';
 
 @Injectable({
@@ -20,10 +20,19 @@ export class RecordarPassService {
   }
 
   recordarPass(recover: IRecover) {
-    const body = { token: recover.token, newPass: recover.newPass };
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', 
+    });
+    const body = {
+      Token: recover.token, 
+      NewPass: recover.newPass 
+    };
     return this.http.post(
       `${this.API_URL}/ChangePasswordControllers/changePasswordMailConEnlace`,
-      body
+      body,
+      { headers: headers ,
+        responseType: 'text' //Si la respuesta llega de backend como texto, angular no lo va a parsear a json, por lo que hay que especificar que es texto
+      }
     );
   }
 }
