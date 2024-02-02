fa6a2afe0ec386c9741ba4bbf3ab7e950ade6fe1
David Galvan Rojas <galvanrojasdavid@gmail.com>
2024-02-02 12:18:28 +0100

    .
    ..

--- DiabetesNoteBook/DiabetesNoteBook/obj/Debug/net7.0/DiabetesNoteBook.AssemblyInfo.cs
+++ DiabetesNoteBook/DiabetesNoteBook/obj/Debug/net7.0/DiabetesNoteBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiabetesNoteBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85766783012f0e1267d9cce6c6cdad190294d0a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682977b9de4d3c35893d042ac9036b46ab9e44db")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiabetesNoteBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiabetesNoteBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
