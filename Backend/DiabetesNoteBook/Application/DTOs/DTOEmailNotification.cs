a22cc52c11375cede519ebb9bc6905ea240567ec
JavierOrtiz <112349811+xaby19@users.noreply.github.com>
2024-02-21 11:16:11 +0100

    .

--- Backend/DiabetesNoteBook/Application/DTOs/DTOEmailNotification.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiabetesNoteBook.Application.DTOs
-{
-    public class DTOEmailNotification
-    {
-        public string ToEmail { get; set; } // Email antiguo
-        public string NewEmail { get; set; } // Nuevo email
-    }
-
-}
