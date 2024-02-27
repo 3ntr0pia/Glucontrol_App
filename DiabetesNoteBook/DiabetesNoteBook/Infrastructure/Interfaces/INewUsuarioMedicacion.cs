eea77c4e6b222253d96d38ecb913f43adb5865a8
David Galvan Rojas <galvanrojasdavid@gmail.com>
2024-02-27 10:06:24 +0100

    .
    .

---
+++ DiabetesNoteBook/DiabetesNoteBook/Infrastructure/Interfaces/INewUsuarioMedicacion.cs
@@ -0,0 +1,9 @@
+﻿using DiabetesNoteBook.Domain.Models;
+
+namespace DiabetesNoteBook.Infrastructure.Interfaces
+{
+    public interface INewUsuarioMedicacion
+    {
+        Task SaveNuevaUsuarioMedicacion(UsuarioMedicacion newusuariomedicacion);
+    }
+}
